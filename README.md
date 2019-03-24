d1ade0d42c4204de68c419d823d1b919f74dca66
Neptune Design <fabrice.fortez@gmail.com>
2019-03-24 14:16:49 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# [VS Sequential Number](https://marketplace.visualstudio.com/items?itemName=neptunedesign.vs-sequential-number)
+
+An Visual Studio Code package, to inputs sequential numbers across multiple cursors.
+
+## Screenshot
+
+![Demo](https://raw.githubusercontent.com/NeptuneDesign/vs-sequential-number/master/images/screenshot.gif)
+
+## Syntax Rules
+
+```
+<start> <operator?> <step?>
+```
+
+| Key                                   | Default | Definition                                                                  |
+| :------------------------------------ | :------ | :-------------------------------------------------------------------------- |
+| **start**                             | `1`     | It specifies the number that you start typing an integer.                   |
+| **operator** (optinal)                | `+`     | It specifies the generation rules of consecutive numbers in the `+` or `-`. |
+| **step** (optinal)                    | `1`     | It specifies the integer to be added or subtracted.                         |
+
+## Release Notes
+
+### 0.0.1
+
+- Initial release
---
+++ extension.js
@@ -0,0 +1,61 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	context.subscriptions.push(vscode.commands.registerCommand('vs-sequential-number.generate', sequentialNumberGenerate));
+}
+exports.activate = activate;
+
+function sequentialNumberGenerate() {
+	if (vscode.window.activeTextEditor) {
+
+		let input = vscode.window.showInputBox({
+			'value': '1',
+			'placeHolder': '1 + 1',
+			'prompt' : '<start> <operator?> <step?>'
+		});
+
+		input.then((value) => {
+			if (value) {
+				let options = [];
+
+				value.split(' ').forEach((element) => {
+					if (element) {
+						options.push(element);
+					}
+				});
+
+				let start = options[0] ? options[0] : '1';
+				let operator = '+';
+				let step = '1';
+
+				if (options[1]) {
+					if (options[1] == '+' || options[1] == '-') {
+						operator = options[1];
+						step = options[2] ? options[2] : '1';
+					} else {
+						step = options[1];
+					}
+				}
+
+				vscode.window.activeTextEditor.edit((editBuilder) => {
+					vscode.window.activeTextEditor.selections.forEach((element, index) => {
+						editBuilder.replace(
+							element,
+							(eval(parseInt(start) + operator + (index * parseInt(step)))).toString()
+						);
+					});
+				});
+			}
+		});
+	}
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
